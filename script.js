77f6f331395b75890ff19f50e053463231970eca
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-06 10:07:31 -0600

    Access Multi-Dimensional Arrays test

--- script.js
+++ script.js
@@ -52,3 +52,12 @@ const myStr = "This is the start." + "This is the end.";
 //
 let myStr1 = "This is the first sentence. ";
 myStr1 += "This is the second sentence.";
+//
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
