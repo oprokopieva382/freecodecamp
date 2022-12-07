169810de8d5096683e8260303e2b8fceca45e2d5
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-07 10:34:49 -0600

    Comparisons with the Logical And Operator

--- script.js
+++ script.js
@@ -158,3 +158,12 @@ function testLessOrEqual(val) {
 }
 
 testLessOrEqual(10);
+//Comparisons with the Logical And Operator
+function testLogicalAnd(val) {
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+  return "No";
+}
+
+testLogicalAnd(10);
