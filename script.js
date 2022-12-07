ffce9e0153c3d867a18604b47901e74699a20ade
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-07 10:28:22 -0600

    Comparison with the Greater Than Operator

--- script.js
+++ script.js
@@ -94,10 +94,25 @@ testNotEqual(99);
 //Comparison with the Strict Inequality Operator
 function testStrictNotEqual(val) {
   if (val !== 17) {
-    // Change this line
     return "Not Equal";
   }
   return "Equal";
 }
 
 testStrictNotEqual(17);
+//Comparison with the Greater Than Operator
+function testGreaterThan(val) {
+  if (val > 100) {
+    // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
