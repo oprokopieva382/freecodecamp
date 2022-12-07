721b4fd387a1e563d3abdd2c8757a1a79920aade
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-07 10:37:51 -0600

    Comparisons with the Logical Or Operator

--- script.js
+++ script.js
@@ -167,3 +167,12 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(10);
+//Comparisons with the Logical Or Operator
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+  return "Inside";
+}
+
+testLogicalOr(15);
