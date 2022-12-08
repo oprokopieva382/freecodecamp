55fdce32e3cc9d004f06c3f6c53e05688d7eaa10
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-08 08:26:45 -0600

    Used the Conditional (Ternary) Operator

--- script.js
+++ script.js
@@ -362,3 +362,9 @@ function convertToInteger(str) {
 }
 
 convertToInteger("10011");
+//Use the Conditional (Ternary) Operator
+function checkEqual(a, b) {
+  return a === b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
