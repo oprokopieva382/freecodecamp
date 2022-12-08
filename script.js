628a42f08a79051a55288d8b8ec55e8f354cdace
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-07 18:57:21 -0600

    Selecting from Many Options with Switch Statements

--- script.js
+++ script.js
@@ -176,3 +176,24 @@ function testLogicalOr(val) {
 }
 
 testLogicalOr(15);
+//Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+  let answer = "";
+  switch (val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
+  return answer;
+}
+
+caseInSwitch(1);
