b645e8ef58ed0f9a5a045ebf642e2bbdc4c34cc3
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-07 22:47:00 -0600

    Nesting For Loops test

--- script.js
+++ script.js
@@ -262,3 +262,18 @@ let total = 0;
 for (let i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
+//Nesting For Loops
+function multiplyAll(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  return product;
+}
+multiplyAll([
+  [1, 2],
+  [3, 4],
+  [5, 6, 7],
+]);
