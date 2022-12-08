664caf54f3c3f5f13d9f738b81f8a93ab70e838d
Oksana(KsyFate) <oprokopieva382@gmail.com>
2022-12-08 08:41:21 -0600

    Used Recursion to Create a Countdown

--- script.js
+++ script.js
@@ -374,3 +374,13 @@ function checkSign(num) {
 }
 
 checkSign(10);
+//Use Recursion to Create a Countdown
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    let countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
